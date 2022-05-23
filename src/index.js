65e51c564e0c04239e3eaec8599446555585cfc3
Alex Miller <codex.nz@gmail.com>
2022-05-24 10:19:56 +1200

    Move index.js to src.
    Signed-off-by: Alex Miller <codex.nz@gmail.com>

--- src/index.js
+++ src/index.js
@@ -15,36 +15,38 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) Patrick Dwyer. All Rights Reserved.
 
-const fs = require('fs');
-const core = require('@actions/core');
-const execSync = require('child_process').execSync;
+const fs = require("fs");
+const core = require("@actions/core");
+const execSync = require("child_process").execSync;
 
 try {
   // check it CycloneDX is installed
   try {
-    execSync('cyclonedx-bom --help');
+    execSync("cyclonedx-bom --help");
   } catch (error) {
-    console.log('Installing CycloneDX...');
-    let output = execSync('npm install -g @cyclonedx/bom', { encoding: 'utf-8' });
+    console.log("Installing CycloneDX...");
+    let output = execSync("npm install -g @cyclonedx/bom", {
+      encoding: "utf-8",
+    });
     console.log(output);
   }
 
-  const path = core.getInput('path');
-  const out = core.getInput('output');
+  const path = core.getInput("path");
+  const out = core.getInput("output");
 
-  console.log('Options:');
+  console.log("Options:");
   console.log(`  path: ${path}`);
   console.log(`  output: ${out}`);
 
-  let command = `cyclonedx-bom --output=${out} ${path}`
+  let command = `cyclonedx-bom --output=${out} ${path}`;
 
   console.log(`Running: ${command}`);
 
-  output = execSync(command, { encoding: 'utf-8' });
+  output = execSync(command, { encoding: "utf-8" });
   console.log(output);
 
-  console.log('BOM Contents:');
-  let bomContents = fs.readFileSync(`${out}`).toString('utf8');
+  console.log("BOM Contents:");
+  let bomContents = fs.readFileSync(`${out}`).toString("utf8");
   console.log(bomContents);
 } catch (error) {
   core.setFailed(error.message);
